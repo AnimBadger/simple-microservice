c27a644eacac0103e4dac2f36c91a9e10b847448
AnimBadger <anim.ansah.stephen@gmail.com>
2024-03-22 18:32:21 +0000

    externalized properties to config server

--- config-server/src/main/java/dev/stephen/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/dev/stephen/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package dev.stephen.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
